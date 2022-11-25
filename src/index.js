8b496612a037ee9147abb69aea3baed474ede682
AntonKh <leaterood@gmail.com>
2022-11-25 15:11:58 +0300

    fix details

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
 import { PixabayApi } from './js/fetch.js'
 
 const formRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
 
-const pixabayAPi = new PixabayApi();
-
-pixabayAPi.fetchImg();
+const pixabayApi = new PixabayApi();
 
 formRef.addEventListener('submit', onFormSearch);
 
 function onFormSearch(event) { 
     event.preventDefault();
-    console.log("hello");
-    const request = event.target.elements.searchQuery.value;
-    pixabayAPi.fetchImg(request)
-        .then(response => creatList(response))
-        .then(response => console.log(response))
-        .catch(err => console.log(err));
+    pixabayApi.request = event.target.elements.searchQuery.value;
+    pixabayApi.fetchImg()
+        .then(response => {
+            console.log(response);
+            // galleryRef.innerHTML = '';
+
+        }).catch(err => console.log(err));
 }
-
-// fetchCountries(event.target.value.trim())
-// .then(data => {
-//     if (data.length >= 10) {
-//             Notify.info('Too many matches found. Please enter a more specific name.');
-//     } else if (data.length > 1 && data.length < 10) {
-//         countryInfoRef.innerHTML = '';
-//         creatList(data);
-//     } else if (data.length === 1) { 
-//         creatCard(data[0]);
-//     }
-// })     
-// .catch(err => { 
-//     if (err.message === '404') { 
-//             Notify.failure('Введите правильное название страны');
-//     }
-// })
-// }
-
-// function creatList(list) { 
-    
-    //  for (const el of list) {      
-    //     const listItem = document.createElement('li');
-    //     countryListRef.append(listItem);
-    //     listItem.classList.add('item_sm');
-    //     listItem.innerHTML = `<img src='${el.flags.png}' width='50'></img><span>${el.name.official}</span>`;
-    // }
-// }
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,17 +1,17 @@
 'use strict';
 
 export class PixabayApi { 
-
     #BASE_URL = 'https://pixabay.com/api';
     #API_KEY = '31540043-77ba2a95f38c13f834341b2a6';
 
     constructor() {
         this.page = 1;
+        this.request = null;
     }
     
     fetchImg() {
         const searchParams = new URLSearchParams ({
-            q: 'cats',
+            q: this.request,
             page: this.page,
             per_page: 40,
             orientation: 'horizontal',
@@ -20,12 +20,12 @@ export class PixabayApi {
             key: this.#API_KEY,
         });
 
-        fetch(`${this.#BASE_URL}/?${searchParams}`)
+        return fetch(`${this.#BASE_URL}/?${searchParams}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
             return response.json();
-        })
+        });
     }
 }
